8bcff4ad714e2e7f223be4c7eb22d6c9d9ea7dd1
Wanting <wantingtengtwt@gmail.com>
2024-01-14 18:20:20 -0500

    new project begins

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+//initialize first
+import "reset-css"
+//UI framework
+
+//global style
+import "./assets/styles/global.scss"
+// component style
+import App from './App'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
